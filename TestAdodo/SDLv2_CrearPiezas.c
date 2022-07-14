5ab57d46fb6962345ce1ef347b27a476fa30493e
Adodo <alonbustos2021@udec.cl>
2022-07-14 19:31:19 -0400

    Crear Piezas

--- TestAdodo/SDLv2_CrearPiezas.c
+++ TestAdodo/SDLv2_CrearPiezas.c
@@ -1,190 +1,244 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
-#include <stdbool.h>
 
 // Constantes
 #define WIDTH 600
 #define HEIGHT 690
-#define TILE_SIZE 22;
+#define TILE_SIZE 35
 
 bool running;
 
-SDL_Renderer* renderer;
-SDL_Window* window;
-
 int frameCount, timerFPS, lastFrame, fps;
-bool left, right, up, down;
+// bool left, right, up, down, rotate, drop;
 
-typedef struct Bloque {
- SDL_Color color;
- bool active;
-} block;
-
+// Estructura de piezas
 typedef struct Forma {
  SDL_Color color;
  bool matrix[4][4];
  double x, y;
  int size;
-} shape; 
+} shape;
 
-shape blocks[7] = {{{255,165,0},
-{{0,0,1,0} // L BLOCK
-,{1,1,1,0}
-,{0,0,0,0}
-,{0,0,0,0}
-},5,4,3}
-,{{255,0,0}, // Z BLOCK
-{{1,1,0,0}
-,{0,1,1,0}
-,{0,0,0,0}
-,{0,0,0,0}
-},5,4,3}
-,{{224,255,255}, // I BLOCK
-{{1,1,1,1}
-,{0,0,0,0}
-,{0,0,0,0}
-,{0,0,0,0}
-},5,4,4}
-,{{0,0,255}, // J BLOCK
-{{1,0,0,0}
-,{1,1,1,0}
-,{0,0,0,0}
-,{0,0,0,0}
-},5,4,3}
-,{{255,255,0}, // O BLOCK
-{{1,1,0,0}
-,{1,1,0,0}
-,{0,0,0,0}
-,{0,0,0,0}
-},5,4,2}
-,{{0,0,255}, // S BLOCK
-{{0,1,1,0}
-,{1,1,0,0}
-,{0,0,0,0}
-,{0,0,0,0}
-},5,4,3}
-,{{128,0,128}, // T BLOCK
-{{0,1,0,0}
-,{1,1,1,0}
-,{0,0,0,0}
-,{0,0,0,0}
-},5,4,3}}, cur;
+// Arreglo de tetrominos
+shape blocks[7] = {
+	// L BLOCK
+	{{255,127,0}, // Color Naranjo
+	{{0,0,1,0} 
+	,{1,1,1,0}
+	,{0,0,0,0}
+	,{0,0,0,0}}
+	,7.5,7.5,3}
+	// Z BLOCK
+	,{{255,0,0}, // Color Rojo
+	{{1,1,0,0}
+	,{0,1,1,0}
+	,{0,0,0,0}
+	,{0,0,0,0}
+	},7.5,7.5,3}
+	// I BLOCK
+	,{{0,255,255}, // Color Celeste
+	{{1,1,1,1}
+	,{0,0,0,0}
+	,{0,0,0,0}
+	,{0,0,0,0}
+	},7.5,7.5,4}
+	// J BLOCK
+	,{{0,0,255}, // Color Azul
+	{{1,0,0,0}
+	,{1,1,1,0}
+	,{0,0,0,0}
+	,{0,0,0,0}
+	},7.5,7.5,3}
+	// O BLOCK
+	,{{255,255,0}, // Color Amarillo
+	{{1,1,0,0}
+	,{1,1,0,0}
+	,{0,0,0,0}
+	,{0,0,0,0}
+	},7.5,7.5,2}
+	// S BLOCK
+	,{{0,255,0}, // Color Verde
+	{{0,1,1,0}
+	,{1,1,0,0}
+	,{0,0,0,0}
+	,{0,0,0,0}
+	},7.5,7.5,3}
+	// T BLOCK
+	,{{128,0,128}, // Color Morado
+	{{0,1,0,0}
+	,{1,1,1,0}
+	,{0,0,0,0}
+	,{0,0,0,0}
+	},7.5,7.5,3}
+};
 
 shape reverseCols(shape s) {
-    shape tmp = s;
-    for(int i = 0; i < s.size; i++) {
-        for(int j = 0; j < s.size/2; j++) {
-            bool t = s.matrix[i][j];
-            tmp.matrix[i][j] = s.matrix[i][s.size-j-1];
-            tmp.matrix[i][s.size-j-1] = t;
-        }
-    }
-    return tmp;
+	shape tmp = s;
+	for(int i = 0; i < s.size; i++) {
+		for(int j = 0; j < s.size/2; j++) {
+			bool t = s.matrix[i][j];
+			tmp.matrix[i][j] = s.matrix[i][s.size-j-1];
+			tmp.matrix[i][s.size-j-1] = t;
+		}
+	}
+	return tmp;
 }
-shape transpose(shape s) {
-    shape tmp = s;
-    for(int i = 0; i < s.size; i++) {
-        for(int j = 0; j < s.size; j++) {
-            tmp.matrix[i][j]=s.matrix[j][i];
-        }
-    }
-    return tmp;
+shape transpose(shape* s) {
+	shape tmp = *s;
+	for(int i = 0; i < s->size; i++) {
+		for(int j = 0; j < s->size; j++) {
+			tmp.matrix[i][j] = s->matrix[j][i];
+		}
+	}
+	return tmp;
 }
-void rotate() {
-    cur =reverseCols(transpose(cur));
+void rotation(shape* s) {
+	*s = reverseCols(transpose(s));
 }
-
-SDL_Rect rect;
-void draw(shape s) {
-    for(int i = 0; i < s.size; i++) {
-        for(int j = 0; j < s.size; j++) {
-            if(s.matrix[i][j]) {
-                rect.x=(s.x+i)*TILE_SIZE; rect.y=(s.y+j)*TILE_SIZE;
-                SDL_SetRenderDrawColor(renderer, s.color.r, s.color.g, s.color.b, 255);
-                SDL_RenderFillRect(renderer, &rect);
-                SDL_SetRenderDrawColor(renderer, 219, 219, 219, 255);
-                SDL_RenderDrawRect(renderer, &rect);
-            }
-        }
-    }
-}
-
-void update() {
-    if(left) cur.x--;
-    if(right) cur.x++;
-    if(down) cur.y++;
-    if(up) rotate();
+void drop(shape* s) {
+	// *s = blocks[rand() % 7];
+	// Numero de pieza aleatoria
+	int randomBlock = rand() % 7; 
+	// Asignar color
+	s->color = blocks[randomBlock].color;
+	// Asignar forma
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			s->matrix[i][j] = blocks[randomBlock].matrix[i][j];
+		}
+	}
+	// Asignar tamaño
+	s->size = blocks[randomBlock].size;
 }
 
-void input() {
-    up = down = left = right = 0;
-    SDL_Event e;
-    while(SDL_PollEvent(&e)) {
-        if(e.type == SDL_QUIT) running = false;
-        switch(e.type) {
-            case SDL_KEYUP: //DOWN
-                switch(e.key.keysym.sym) {
-                    case SDLK_LEFT:
-                        left = 1;
-                        break;
-                    case SDLK_RIGHT:
-                        right = 1;
-                        break;
-                    case SDLK_UP:
-                        up = 1;
-                        break;
-                    case SDLK_DOWN:
-                        down = 1;
-                        break;
-                    case SDLK_ESCAPE:
-                        running = false;
-                        break;
-                }
-        }
-    }
+void draw(shape* s, SDL_Rect* rect, SDL_Renderer* renderer) {
+	for(int i = 0; i < s->size; i++) {
+		for(int j = 0; j < s->size; j++) {
+			if(s->matrix[i][j]) {
+				rect->x = (s->x + i) * TILE_SIZE;
+				rect->y = (s->y + j) * TILE_SIZE;
+				SDL_SetRenderDrawColor(renderer, s->color.r, s->color.g, s->color.b, 255); // Escoger color para cuadrados
+				SDL_RenderFillRect(renderer, rect); // Pintar Cuadrados
+				SDL_SetRenderDrawColor(renderer, 219, 219, 219, 255); // Escoger color para contorno (Gris)
+				// SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Escoger color para contorno (Blanco)
+				SDL_RenderDrawRect(renderer, rect); // Pintar contorno
+			}
+		}
+	}
 }
 
-void render() {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
+// void update(shape* cur) {
+// 	if (left) cur->x--;
+// 	if (right) cur->x++;
+// 	if (down) cur->y++;
+// 	if (up) cur->y--;
+// 	if (rotate) rotation();
+// 	// if (drop);
+// }
 
-    frameCount++;
-    int timerFPS = SDL_GetTicks() - lastFrame;
-    if (timerFPS < (1000/60)) {
-        SDL_Delay((1000/60) - timerFPS);
-    }
+void input(shape* cur) {
+	// up = down = left = right = rotate = drop = 0;
+	SDL_Event e;
+	while(SDL_PollEvent(&e)) {
+		if(e.type == SDL_QUIT) running = false;
+		switch(e.type) {
+			// case SDL_KEYUP: //DOWN
+			case SDL_KEYDOWN:
+				switch(e.key.keysym.sym) {
+					case SDLK_a:
+					case SDLK_LEFT:
+						// left = 1;
+						cur->x--;
+						break;
+					case SDLK_d:
+					case SDLK_RIGHT:
+						// right = 1;
+						cur->x++;
+						break;
+					case SDLK_w:
+					case SDLK_UP:
+						// up = 1;
+						cur->y--;
+						break;
+					case SDLK_s:
+					case SDLK_DOWN:
+						// down = 1;
+						cur->y++;
+						break;
+					case SDLK_z:
+						// rotate = 1;
+						rotation(cur);
+						break;
+					case SDLK_SPACE:
+						drop(cur);
+						// drop = 1;
+						break;
+					case SDLK_ESCAPE:
+						running = false;
+						break;
+				}
+		}
+	}
+}
 
-    draw(cur);
+void render(SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
 
-    SDL_RenderPresent(renderer);
+	frameCount++;
+	int timerFPS = SDL_GetTicks() - lastFrame;
+	if (timerFPS < (1000/60)) {
+		SDL_Delay((1000/60) - timerFPS);
+	}
 }
 
 int main() {
-    srand(time(NULL));
-    cur = blocks[rand() % 7];
-    rect.w = rect.h = TILE_SIZE;
-    running = 1;
-    static int lastTime = 0;
-    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0");
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) printf("Error inicializando SDL: %s\n", SDL_GetError());
-    if (SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer) != 0) printf("Error al intentar SDL_CreateWindowAndRenderer(): %s\n", SDL_GetError());
-    SDL_SetWindowTitle(window, "Tetris");
+	srand(time(NULL));
 
-    while(running) {
-        lastFrame=SDL_GetTicks();
-        if(lastFrame>=(lastTime+1000)) {
-            lastTime=lastFrame;
-            fps=frameCount;
-            frameCount=0;
-        }
+	shape cur = blocks[rand() % 7];
 
-        update();
-        input();
-        render();
-    }
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+	SDL_Rect rect;
+	rect.w = rect.h = TILE_SIZE;
+
+	running = 1;
+	static int lastTime = 0;
+
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0");
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		printf("Error inicializando SDL: %s\n", SDL_GetError());
+	}
+
+	SDL_Renderer* renderer;
+	SDL_Window* window;
+	if (SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer) != 0) {
+		printf("Error al intentar SDL_CreateWindowAndRenderer(): %s\n", SDL_GetError());
+	}	
+	SDL_SetWindowTitle(window, "Intento de Tetris");
+	SDL_Surface* icon = IMG_Load("udec_icon.webp");
+	SDL_SetWindowIcon(window, icon);
+	SDL_FreeSurface(icon);
+
+
+	while(running) {
+		lastFrame = SDL_GetTicks();
+		if(lastFrame >= (lastTime+1000)) {
+			lastTime = lastFrame;
+			fps = frameCount;
+			frameCount = 0;
+		}
+
+		// update(&cur);
+		input(&cur);
+		render(renderer);
+
+		draw(&cur, &rect, renderer);
+		SDL_RenderPresent(renderer);
+	}
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
